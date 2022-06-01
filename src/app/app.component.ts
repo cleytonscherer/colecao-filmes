271cd9e3884440a0fbc555d8cac76fb3ab7320ef
Cleyton Scherer <cleytonscherer@gmail.com>
2022-05-31 21:12:23 -0300

    Atividade 06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+//import * as $ from 'jquery';
+import * as M from 'materialize-css';
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'colecao-filmes';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('mobile') sideNav?: ElementRef;
+
+  ngAfterViewInit(): void {
+    //let $sideNav = $('#mobile-demo');
+    //M.Sidenav.init($sideNav);
+    M.Sidenav.init(this.sideNav?.nativeElement);
+  }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  imagem_logo = './assets/resources/images/logo.png';
+
+  cadFilmeDesc = 'Cadastrar Filme';
+  cadFilmeUrl  = 'cad-filme.html';
+
+  listarFilmesDesc = 'Listar Filmes';
+  listarFilmesUrl = 'list-filmes.html';
+
+  cadGeneroDesc = 'Cadastrar Gênero';
+  cadGeneroUrl = 'cad-genero.html';
+
+  listarGenerosDesc = 'Listar Gêneros';
+  listarGenerosUrl = 'list-generos.html';
+
+  sobreDesc = 'Sobre';
+  sobreUrl = 'sobre.html';
+  
   constructor() { }
 
   ngOnInit(): void {
